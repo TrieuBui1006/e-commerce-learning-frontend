971546276fc3becc044abafbc97cd5e7a82da035
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-08-01 16:41:14 +0200

    implement shared  layout

---
+++ src/user/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Signin = () => {
+  return (
+    <Layout title="Sign In" description="Sign in to Node React E-commerce">
+      ...
+    </Layout>
+  )
+}
+
+export default Signin
---
+++ src/user/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Signup = () => {
+  return (
+    <Layout title="Sign up" description="Sign up to Node React E-commerce">
+      ...
+    </Layout>
+  )
+}
+
+export default Signup
