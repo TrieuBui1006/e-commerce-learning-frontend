bdc63df7d11f601fa04ba03a303661b259a38ce4
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-08-02 10:48:14 +0200

    private route

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import PrivateRoute from './auth/PrivateRoute'
 
 import Signup from './user/Signup'
 import Signin from './user/Signin'
 import Home from './core/Home'
+import Dashboard from './user/UserDashboard'
 
 const Routes = () => {
   return (
@@ -13,6 +15,7 @@ const Routes = () => {
           <Route path="/" exact component={Home} />
           <Route path="/signin" exact component={Signin} />
           <Route path="/signup" exact component={Signup} />
+          <PrivateRoute path="/dashboard" exact component={Dashboard} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -19,6 +19,15 @@ const Menu = ({ history }) => {
             Home
           </Link>
         </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            to="/dashboard"
+            style={isActive(history, '/dashboard')}
+          >
+            Dashboard
+          </Link>
+        </li>
         {!isAuthenticated() && (
           <>
             <li className="nav-item">
