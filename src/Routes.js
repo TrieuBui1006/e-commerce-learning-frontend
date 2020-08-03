f056a5d45f05792beb47620ec371b119eb95dcee
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-08-03 19:07:30 +0200

    implement categories in addProduct

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import Home from './core/Home'
 import Dashboard from './user/UserDashboard'
 import AdminDashboard from './user/AdminDashboard'
 import AddCategory from './admin/AddCategory'
+import AddProduct from './admin/AddProduct'
 
 const Routes = () => {
   return (
@@ -25,6 +26,7 @@ const Routes = () => {
           />
           <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
           <AdminRoute path="/create/category" exact component={AddCategory} />
+          <AdminRoute path="/create/product" exact component={AddProduct} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -17,3 +17,30 @@ export const createCategory = async (userId, token, category) => {
     console.log(err)
   }
 }
+
+export const createProduct = async (userId, token, product) => {
+  // console.log(name, email, password)
+  try {
+    const res = await fetch(`${API}/product/create/${userId}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: product,
+    })
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: 'GET',
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
+}
